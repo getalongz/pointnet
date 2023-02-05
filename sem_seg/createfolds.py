cc571f63154b50683df07082e5721d2cfc2ee8c6
getalongz <49867440+getalongz@users.noreply.github.com>
2023-02-05 17:23:44 +0800

    Update createfolds.py

--- sem_seg/createfolds.py
+++ sem_seg/createfolds.py
@@ -1,10 +1,10 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if not os.path.exists(BASE_DIR+"\\annotations"):
-    os.makedirs(BASE_DIR+"\\annotations")
+if not os.path.exists(BASE_DIR+"/annotations"):
+    os.makedirs(BASE_DIR+"/annotations")
 
 for i in range(0,100):
-    path = BASE_DIR+"\\annotations\\"+ str(i)
+    path = BASE_DIR+"/annotations/"+ str(i)
     print(path)
     os.mkdir(path)
